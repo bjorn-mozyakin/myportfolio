0ec477cc9f81c53fb3ef96a8a917d1eacc4658b7
Rakov Dmitry <rakov.di@gmail.com>
2015-08-26 12:23:02 +0300

    Add slider for Latest Work - script for Arrows

--- portfolio/layout2-BisLite/js/scripts.js
+++ portfolio/layout2-BisLite/js/scripts.js
@@ -54,11 +54,59 @@ function handler() {
     var strNew = '[data-number="' + newNumber + '"]';
     document.querySelector(strCurrent).classList.remove('active');
     document.querySelector(strNew).classList.add('active');
-
   }
 
+  function Arrows(options) {
+    this.elem = options.elem;
+    this.worksCount = options.worksCount;
+    this.currentMargin = 0;
+    this.worksOnMainPage = 4;
+    this.marginStep = 250;
+    this.maxMargin = this.marginStep * (this.worksCount - this.worksOnMainPage);
+
+    this.elem.onclick = this.onclick.bind(this);
+  }
+
+  Arrows.prototype.onclick = function(e) {
+    this.changeWork(e);
+  }
+
+  Arrows.prototype.changeWork = function(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
+
+    if (target.classList.contains('arrow-right')) {
+      if (this.currentMargin > 0 - this.maxMargin) {
+        document.querySelector('.arrow-left').style.background = '';
+        this.currentMargin -= this.marginStep;
+        document.querySelector('.works-container').style.marginLeft = this.currentMargin + 'px';
+      }
+      if (this.currentMargin == 0 - this.maxMargin) {
+        target.style.background = 'url("img/icons/arrow-right-disabled.png")';
+      }
+    }
+
+    if (target.classList.contains('arrow-left')) {
+      if (this.currentMargin < 0) {
+        document.querySelector('.arrow-right').style.background = '';
+        this.currentMargin += this.marginStep;
+        document.querySelector('.works-container').style.marginLeft = this.currentMargin + 'px';
+      }
+      if (this.currentMargin == 0) {
+        target.style.background = 'url("img/icons/arrow-left-disabled.png")';
+      }
+    }
+  }
+
+  document.querySelector('.arrow-left').style.background = 'url("img/icons/arrow-left-disabled.png")';
+
+  var arrows = new Arrows ({
+    elem: document.querySelector('.arrows'),
+    worksCount: document.querySelector('.works-container').querySelectorAll('img').length
+  })
+
   var slider = new Slider({
-    elem: document.querySelector('.slider')
+    elem: document.querySelector('.slider'),
   });
 
   var timer = setInterval(slider.carusel, 5000);
